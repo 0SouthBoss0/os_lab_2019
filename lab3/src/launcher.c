822a94da3ceb32228d76e1b81fb1f052cf9b08db
Mikhail Trusov <56502446+0SouthBoss0@users.noreply.github.com>
2024-10-19 09:13:28 +0000

    lab3 past-done

--- lab3/src/launcher.c
+++ lab3/src/launcher.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <unistd.h>
+
 #include <sys/wait.h>
 
-int main(int argc, char **argv){
-  const char* NameScript = "./sequential_min_max";
-  char *args[4] = { " ", "5", "8", NULL};
+int main(int argc, char ** argv) {
+  const char *NameScript = "./sequential_min_max";
+  char *args[4] = {" ", "5", "8", NULL};
   int rv, status;
   int pid = fork();
   if (pid == 0) {
     execvp(NameScript, args);
     exit(rv);
+  } else {
+    if (wait(&status) >= 0) {
+      if (WIFEXITED(status)) {
+        printf("Child process exited with %d status\n", WEXITSTATUS(status));
+      }
+    }
   }
-  else{
-    wait(&status);
-  }  
   fflush(NULL);
   return 0;
 }
